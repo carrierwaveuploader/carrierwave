b4ae48133bb28d7edabebcc214ce80934aeba5ae
Jonas Nicklas <jonas.nicklas@gmail.com>
2009-01-29 08:43:36 +0100

    added activerecord plugin

---
+++ more/activerecord/lib/merb_upload_activerecord.rb
@@ -0,0 +1,18 @@
+# make sure we're running inside Merb
+if defined?(Merb::Plugins)
+
+  # Merb gives you a Merb::Plugins.config hash...feel free to put your stuff in your piece of it
+  Merb::Plugins.config[:merb_upload_activerecord] = {
+    :chickens => false
+  }
+  
+  Merb::BootLoader.before_app_loads do
+    # require code that must be loaded before the application
+  end
+  
+  Merb::BootLoader.after_app_loads do
+    # code that can be required after the application loads
+  end
+  
+  Merb::Plugins.add_rakefiles "merb_upload_activerecord/merbtasks"
+end
