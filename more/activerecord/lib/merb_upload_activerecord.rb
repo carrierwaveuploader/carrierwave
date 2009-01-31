01b97dd8b6d35688b816b25547f6d8386e363549
Jonas Nicklas <jonas.nicklas@gmail.com>
2009-01-31 12:42:20 +0100

    make mounting possible in AR

--- more/activerecord/lib/merb_upload_activerecord.rb
+++ more/activerecord/lib/merb_upload_activerecord.rb
@@ -1,24 +1,36 @@
 module Merb
-  
   module Upload
-    
     module ActiveRecord
       
-      def attach_uploader(uploader)
-        class_eval do
-          def file
-            
-          end
-        end
+      def uploaders
+        @uploaders ||= {}
       end
       
       def mount_uploader(column, uploader)
+        
+        uploaders[column] = uploader
+        
+        class_eval <<-EOF, __FILE__, __LINE__
+        
+          after_save :store_#{column}!
+        
+          def store_#{column}!
+            @#{column}_uploader.store! if @#{column}_uploader
+          end
+        
+          def #{column}
+            @#{column}_uploader
+          end
+          
+          def #{column}=(new_file)
+            @#{column}_uploader ||= self.class.uploaders[:#{column}].new(self, :#{column})
+            @#{column}_uploader.cache!(new_file)
+          end
+        EOF
       end
       
-    end
-    
-  end
-  
-end
+    end # ActiveRecord
+  end # Upload
+end # Merb
 
 ActiveRecord::Base.send(:extend, Merb::Upload::ActiveRecord)
--- more/activerecord/spec/merb_upload_activerecord_spec.rb
+++ more/activerecord/spec/merb_upload_activerecord_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe Merb::Upload::ActiveRecord do
   
-  describe '.attach_uploader' do
+  describe '.mount_uploader' do
     
     before(:all) { TestMigration.up }
     after(:all) { TestMigration.down }
@@ -12,18 +12,52 @@ describe Merb::Upload::ActiveRecord do
       @class = Class.new(ActiveRecord::Base)
       @class.table_name = "events"
       @uploader = Class.new(Merb::Upload::AttachableUploader)
-      @class.attach_uploader(@uploader)
+      @class.mount_uploader(:image, @uploader)
       @event = @class.new
     end
     
-    describe '#file' do
+    describe '#image' do
       
       it "should return nil when nothing has been assigned" do
-        @event.file.should be_nil
+        @event.image.should be_nil
       end
+    
+    end
+    
+    describe '#image=' do
       
       it "should cache a file" do
-        @event.file = stub_file('test.jpeg')
+        @event.image = stub_file('test.jpeg')
+        @event.image.should be_an_instance_of(@uploader)
+      end
+      
+      it "should copy a file into into the cache directory" do
+        @event.image = stub_file('test.jpeg')
+        @event.image.current_path.should =~ /^#{public_path('uploads/tmp')}/
+      end
+      
+    end
+    
+    describe '#save' do
+      
+      it "should do nothing when no file has been assigned" do
+        @event.save.should be_true
+        @event.image.should be_nil
+      end
+      
+      it "should copy the file to the upload directory when a file has been assigned" do
+        @event.image = stub_file('test.jpeg')
+        @event.save.should be_true
+        @event.image.should be_an_instance_of(@uploader)
+        @event.image.current_path.should == public_path('uploads/test.jpeg')
+      end
+      
+      it "should do nothing when a validation fails" do
+        @class.validate { |r| r.errors.add :textfile, "FAIL!" }
+        @event.image = stub_file('test.jpeg')
+        @event.save.should be_false
+        @event.image.should be_an_instance_of(@uploader)
+        @event.image.current_path.should =~ /^#{public_path('uploads/tmp')}/
       end
       
     end
