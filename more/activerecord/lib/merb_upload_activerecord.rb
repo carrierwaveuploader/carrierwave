fe90e0522dc303ae1edf928047af463441b7bd5e
Jonas Nicklas <jonas.nicklas@gmail.com>
2009-02-01 00:17:46 +0100

    bound to database

--- more/activerecord/lib/merb_upload_activerecord.rb
+++ more/activerecord/lib/merb_upload_activerecord.rb
@@ -14,20 +14,33 @@ module Merb
         
         class_eval <<-EOF, __FILE__, __LINE__
         
-          after_save :store_#{column}!
-        
-          def store_#{column}!
-            @#{column}_uploader.store! if @#{column}_uploader
-          end
-        
-          def #{column}
-            @#{column}_uploader
-          end
-          
-          def #{column}=(new_file)
-            @#{column}_uploader ||= self.class.uploaders[:#{column}].new(self, :#{column})
-            @#{column}_uploader.cache!(new_file)
-          end
+          after_save :store_#{column}!                                                        # after_save :store_image!
+                                                                                              # 
+          before_validation :set_#{column}_filename                                           # before_validation :set_image_filename
+                                                                                              # 
+          def store_#{column}!                                                                # def store_image!
+            @#{column}_uploader.store! if @#{column}_uploader                                 #   @image_uploader.store! if @image_uploader
+          end                                                                                 # end
+                                                                                              # 
+          def #{column}                                                                       # def image
+            return @#{column}_uploader if @#{column}_uploader                                 #   return @image_uploader if @image_uploader
+            unless self[:#{column}].blank?                                                    #   unless self[:image].blank?
+              @#{column}_uploader ||= self.class.uploaders[:#{column}].new(self, :#{column})  #     @image_uploader ||= self.class.uploaders[:image].new(self, :image)
+              @#{column}_uploader.retrieve_from_store!(self[:#{column}])                      #     @image_uploader.retrieve_from_store!(self[:image])
+              @#{column}_uploader                                                             #     @image_uploader
+            end                                                                               #   end
+          end                                                                                 # end
+                                                                                              # 
+          def #{column}=(new_file)                                                            # def image=(new_file)
+            @#{column}_uploader ||= self.class.uploaders[:#{column}].new(self, :#{column})    #   @image_uploader ||= self.class.uploaders[:image].new(self, :image)
+            @#{column}_uploader.cache!(new_file)                                              #   @image_uploader.cache!(new_file)
+          end                                                                                 # end
+                                                                                              # 
+          private                                                                             # private
+                                                                                              # 
+          def set_#{column}_filename                                                          # def set_image_filename
+            self[:#{column}] = @#{column}_uploader.filename if @#{column}_uploader            #   self[:image] = @image_uploader.filename if @image_uploader
+          end                                                                                 # end
         EOF
       end
       
--- more/activerecord/spec/merb_upload_activerecord_spec.rb
+++ more/activerecord/spec/merb_upload_activerecord_spec.rb
@@ -21,6 +21,27 @@ describe Merb::Upload::ActiveRecord do
       it "should return nil when nothing has been assigned" do
         @event.image.should be_nil
       end
+      
+      it "should return nil when an empty string has been assigned" do
+        @event[:image] = ''
+        @event.save
+        @event.reload
+        @event.image.should be_nil
+      end
+      
+      it "should retrieve a file from the storage if a value is stored in the database" do
+        @event[:image] = 'test.jpeg'
+        @event.save
+        @event.reload
+        @event.image.should be_an_instance_of(@uploader)
+      end
+      
+      it "should set the path to the store dir" do
+        @event[:image] = 'test.jpeg'
+        @event.save
+        @event.reload
+        @event.image.current_path.should == public_path('uploads/test.jpeg')
+      end
     
     end
     
@@ -29,6 +50,10 @@ describe Merb::Upload::ActiveRecord do
       it "should cache a file" do
         @event.image = stub_file('test.jpeg')
         @event.image.should be_an_instance_of(@uploader)
+      end
+      
+      it "should write nothing to the database, to prevent overriden filenames to fail because of unassigned attributes" do
+        @event[:image].should be_nil
       end
       
       it "should copy a file into into the cache directory" do
@@ -58,6 +83,21 @@ describe Merb::Upload::ActiveRecord do
         @event.save.should be_false
         @event.image.should be_an_instance_of(@uploader)
         @event.image.current_path.should =~ /^#{public_path('uploads/tmp')}/
+      end
+      
+      it "should assign the filename to the database" do
+        @event.image = stub_file('test.jpeg')
+        @event.save.should be_true
+        @event.reload
+        @event[:image].should == 'test.jpeg'
+      end
+      
+      it "should assign the filename before validation" do
+        @class.validate { |r| r.errors.add_to_base "FAIL!" if r[:image].nil? }
+        @event.image = stub_file('test.jpeg')
+        @event.save.should be_true
+        @event.reload
+        @event[:image].should == 'test.jpeg'
       end
       
     end
