c9514454d821bec167084c276af17b713d61c14c
Jonas Nicklas <jonas.nicklas@gmail.com>
2009-02-15 21:19:43 +0100

    Allow running of features under ActiveSupport

--- core/features/env.rb
+++ core/features/env.rb
@@ -2,22 +2,30 @@ $TESTING=true
 $:.push File.join(File.dirname(__FILE__), '..', 'lib')
 
 require 'rubygems'
-require 'extlib'
+
+if ENV["AS"]
+  puts "--> using ActiveSupport"
+  require 'activesupport'
+else
+  puts "--> using Extlib"
+  require 'extlib'
+end
+
 require 'tempfile'
 require 'ruby-debug'
 require 'spec'
 
 require 'merb_upload'
 
-Merb::Upload.config[:public] = File.dirname(__FILE__) / 'public'
-Merb::Upload.config[:store_dir] = File.dirname(__FILE__) / 'public' / 'uploads'
-Merb::Upload.config[:cache_dir] = File.dirname(__FILE__) / 'public' / 'uploads' / 'tmp'
-
 alias :running :lambda
 
 def file_path( *paths )
   File.expand_path(File.join(File.dirname(__FILE__), *paths))
 end
+
+Merb::Upload.config[:public] = file_path('public')
+Merb::Upload.config[:store_dir] = file_path('public', 'uploads')
+Merb::Upload.config[:cache_dir] = file_path('public', 'uploads', 'tmp')
 
 After do
   FileUtils.rm_rf(file_path("public"))
