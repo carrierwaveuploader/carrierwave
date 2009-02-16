78570e0d61002bf8a85f69b8631c556469729d6f
Jonas Nicklas <jonas.nicklas@gmail.com>
2009-02-16 21:48:06 +0100

    moved rmagick and image science into core

--- more/image_science/spec/merb_upload_image_science_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.dirname(__FILE__) + '/spec_helper'
-
-describe "merb_upload_image_science" do
-  it "should do nothing" do
-    true.should == true
-  end
-end
--- more/rmagick/spec/merb_upload_rmagick_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.dirname(__FILE__) + '/spec_helper'
-
-describe "merb_upload_rmagick" do
-  it "should do nothing" do
-    true.should == true
-  end
-end
