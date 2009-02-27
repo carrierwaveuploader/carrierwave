4c53f9c95411d9eab2649557046ffc579935693d
Jonas Nicklas <jonas.nicklas@gmail.com>
2009-02-27 20:48:43 +0100

    Added resize_and_pad to rmagick plus documentation

--- lib/stapler/processing/rmagick.rb
+++ lib/stapler/processing/rmagick.rb
@@ -1,45 +1,157 @@
 require 'rmagick'
 
 module Stapler
+  
+  ##
+  # This module simplifies manipulation with RMagick by providing a set
+  # of convenient helper methods. If you want to use them, you'll need to
+  # require this file
+  #
+  #     require 'stapler/processing/rmagick'
+  #
+  # And then include it in your uploader
+  #
+  #     MyUploade < Stapler::Uploader
+  #       include Stapler::RMagick
+  #     end
+  #
+  # You can now use the provided helpers:
+  #
+  #     MyUploade < Stapler::Uploader
+  #       include Stapler::RMagick
+  #       
+  #       process :resize_to_fit => [200, 200] 
+  #     end
+  #
+  # Or create your own helpers with the powerful manipulate! method. Check
+  # out the RMagick docs at http://www.imagemagick.org/RMagick/doc/ for more
+  # info
+  #
+  #     MyUploade < Stapler::Uploader
+  #       include Stapler::RMagick
+  #       
+  #       process :do_stuff => 10.0
+  #       
+  #       def do_stuff(blur_factor)
+  #         manipulate! do |img|
+  #           img = img.sepiatone
+  #           img = img.auto_orient
+  #           img = img.radial_blur(blur_factor)
+  #         end
+  #       end
+  #     end
+  #
   module RMagick
     
-    # Convert the image to format
+    ##
+    # Changes the image encoding format to the given format
+    #
+    # @see http://www.imagemagick.org/RMagick/doc/magick.html#formats
+    # @param [#to_s] format an abreviation of the format
+    # @yieldparam [Magick::Image] img additional manipulations to perform
+    # @example
+    #     image.convert(:png)
+    # 
     def convert(format)
       manipulate! do |img|
         img.format = format.to_s.upcase
+        img = yield(img) if block_given?
         img
       end
     end
 
-    # Resize the image so that it will not exceed the dimensions passed
-    def resize(height, width)
-      geometry = "#{height}x#{width}"
+    ##
+    # From the RMagick documentation: "Resize the image to fit within the
+    # specified dimensions while retaining the original aspect ratio. The
+    # image may be shorter or narrower than specified in the smaller dimension
+    # but will not be larger than the specified values."
+    #
+    # @see http://www.imagemagick.org/RMagick/doc/image3.html#resize_to_fit
+    #
+    # @param [Integer] width the width to scale the image to
+    # @param [Integer] height the height to scale the image to
+    # @yieldparam [Magick::Image] img additional manipulations to perform
+    # 
+    def resize_to_fit(width, height)
       manipulate! do |img|
-        img.change_geometry( geometry ) do |c, r, i|
-          i.resize(c,r)
+        img.resize_to_fit!(width, height)
+        img = yield(img) if block_given?
+        img
+      end
+    end
+    
+    alias_method :resize, :resize_to_fit
+
+    ##
+    # From the RMagick documentation: "Resize the image to fit within the
+    # specified dimensions while retaining the aspect ratio of the original
+    # image. If necessary, crop the image in the larger dimension."
+    #
+    # @see http://www.imagemagick.org/RMagick/doc/image3.html#resize_to_fill
+    #
+    # @param [Integer] width the width to scale the image to
+    # @param [Integer] height the height to scale the image to
+    # @yieldparam [Magick::Image] img additional manipulations to perform
+    #
+    def resize_to_fill(width, height)
+      manipulate! do |img|
+        img.resize_to_fill!(width, height)
+        img = yield(img) if block_given?
+        img
+      end
+    end
+    
+    alias_method :crop_resized, :resize_to_fill
+    
+    ##
+    # Resize the image to fit within the specified dimensions while retaining
+    # the original aspect ratio. If necessary will pad the remaining area
+    # with the given color, which defaults to transparent (for gif and png,
+    # white for jpeg).
+    #
+    # @param [Integer] width the width to scale the image to
+    # @param [Integer] height the height to scale the image to
+    # @param [String, :transparent] background the color of the background as a hexcode, like "#ff45de"
+    # @param [Magick::GravityType] gravity how to position the image
+    # @yieldparam [Magick::Image] img additional manipulations to perform
+    #
+    def resize_and_pad(width, height, background=:transparent, gravity=::Magick::CenterGravity)
+      manipulate! do |img|
+        img.resize_to_fit!(width, height)
+        new_img = ::Magick::Image.new(width, height)
+        if background == :transparent
+          new_img = new_img.matte_floodfill(1, 1)
+        else
+          new_img = new_img.color_floodfill(1, 1, ::Magick::Pixel.from_color(background))
         end
+        new_img = new_img.composite(img, gravity, ::Magick::OverCompositeOp)
+        new_img = yield(new_img) if block_given?
+        new_img
       end
     end
 
-    # Resize and crop the image so that it will have the exact dimensions passed
-    def crop_resized(height, width)
-      manipulate! do |img|
-        img.crop_resized(height,width)
-      end
-    end
-
-    # Manipulate the image with rmagick
+    ##
+    # Manipulate the image with RMagick. This method will load up an image
+    # and then pass each of its frames to the supplied block. It will then
+    # save the image to disk.
+    #
+    # Note: This method assumes that the object responds to current_path
+    # any class that this is mixed into must have a current_path method.
+    #
+    # @yieldparam [Magick::Image] img manipulations to perform
+    # @raise [Stapler::ProcessingError] if manipulation failed.
+    #
     def manipulate!
-      image = ::Magick::Image.read(self.current_path)
+      image = ::Magick::Image.read(current_path)
 
       if image.size > 1
         list = ::Magick::ImageList.new
         image.each do |frame|
           list << yield( frame )
         end
-        list.write(self.current_path)
+        list.write(current_path)
       else
-        yield( image.first ).write(self.current_path)
+        yield( image.first ).write(current_path)
       end
     rescue ::Magick::ImageMagickError => e
       raise Stapler::ProcessingError.new("Failed to manipulate with rmagick, maybe it is not an image? Original Error: #{e}")
