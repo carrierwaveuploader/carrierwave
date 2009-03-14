e2d7fa628f294f353d7934e6ee0ebe9e0e8edcf1
Jonas Nicklas <jonas.nicklas@gmail.com>
2009-03-15 00:31:12 +0100

    Hint on how to use whitelist in generated template

--- rails_generators/uploader/templates/uploader.rb
+++ rails_generators/uploader/templates/uploader.rb
@@ -20,6 +20,12 @@ class <%= class_name %>Uploader < CarrierWave::Uploader
   #       process :scale => [50, 50]
   #     end
   
+  # Add a white list of extensions which are allowed to be uploaded,
+  # for images you might use something like this:
+  #     def extension_white_list
+  #       %w(jpg jpeg gif png)
+  #     end
+  
   # Override the filename of the uploaded files
   #     def filename
   #       "something.jpg"
