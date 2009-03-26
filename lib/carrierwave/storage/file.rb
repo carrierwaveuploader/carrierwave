940e90aec43177894cff7794afbdac1501bd4daf
Jonas Nicklas <jonas.nicklas@gmail.com>
2009-03-26 12:35:11 +0100

    Use permissions in file store

--- lib/carrierwave/storage/file.rb
+++ lib/carrierwave/storage/file.rb
@@ -27,7 +27,7 @@ module CarrierWave
       def self.store!(uploader, file)
         path = ::File.join(uploader.store_dir, uploader.filename)
         path = ::File.expand_path(path, uploader.public)
-        file.move_to(path)
+        file.move_to(path, CarrierWave.config[:permissions])
         file
       end
       
