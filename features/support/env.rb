11c82d7025ca9e46c57786c52908e85df5de6a60
Jonas Nicklas <jonas.nicklas@gmail.com>
2009-09-19 19:56:50 +0200

    Fixed paths in env.rb.
    Somehow features were run from gem.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,30 +1,18 @@
 # encoding: utf-8
 
 $TESTING=true
-$:.push File.join(File.dirname(__FILE__), '..', 'lib')
+$:.unshift File.expand_path(File.join('..', '..', 'lib'), File.dirname(__FILE__))
 
-require 'rubygems'
 require File.join(File.dirname(__FILE__), 'activerecord')
 require File.join(File.dirname(__FILE__), 'datamapper')
 
-if ENV["AS"]
-  puts "--> using ActiveSupport"
-  require 'activesupport'
-elsif ENV["EXTLIB"]
-  puts "--> using Extlib"
-  require 'extlib'
-end
-
-require 'tempfile'
-#require 'ruby-debug'
 require 'spec'
-
 require 'carrierwave'
 
 alias :running :lambda
 
 def file_path( *paths )
-  File.expand_path(File.join(File.dirname(__FILE__), '..', *paths))
+  File.expand_path(File.join('..', *paths), File.dirname(__FILE__))
 end
 
 CarrierWave.config[:root] = file_path('public')
