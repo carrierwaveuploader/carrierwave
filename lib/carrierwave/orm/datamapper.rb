2337fe4ddfd970fae9433356a6babc01ff15b304
Quin Hoxie <qhoxie@gmail.com>
2009-11-14 22:26:19 -0800

    DataMapper adapter: fix scoping of DM constant and add specs for validations in DM context.

--- lib/carrierwave/orm/datamapper.rb
+++ lib/carrierwave/orm/datamapper.rb
@@ -15,9 +15,8 @@ module CarrierWave
 
       alias_method :read_uploader, :attribute_get
       alias_method :write_uploader, :attribute_set
-
       after :save, "store_#{column}!".to_sym
-      pre_hook = DataMapper.const_defined?(:Validate) ? :valid? : :save
+      pre_hook = ::DataMapper.const_defined?(:Validate) ? :valid? : :save
       before pre_hook, "write_#{column}_identifier".to_sym
       after :destroy, "remove_#{column}!".to_sym
     end
