f22c5d631b6a1615c66acf84161ee559c0c51762
Andreas Haller <andreashaller@gmail.com>
2010-03-18 02:46:05 +0100

    make the rails 3 generator work
    seems like you have to overwrite source_root.

--- lib/generators/uploader_generator.rb
+++ lib/generators/uploader_generator.rb
@@ -5,5 +5,9 @@ class UploaderGenerator < Rails::Generators::NamedBase
   def create_uploader_file
     template 'uploader.rb', File.join('app/uploaders', class_path, "#{file_name}.rb")
   end
+  
+  def self.source_root
+    @source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+  end
 end
 
