ecabc618d0fce22c1931c6d2eb134886e3b60e4c
Jonas Nicklas <jonas.nicklas@gmail.com>
2010-07-20 22:56:55 +0200

    Reload record in mongoid spec, proves bug doesn't exist in Mongoid, closes #72

--- spec/orm/mongoid_spec.rb
+++ spec/orm/mongoid_spec.rb
@@ -149,6 +149,7 @@ describe CarrierWave::Mongoid do
           @doc.save
           @doc.remove_image = true
           @doc.save
+          @doc.reload
           @doc.image.should be_blank
           @doc.image_filename.should == ''
         end
