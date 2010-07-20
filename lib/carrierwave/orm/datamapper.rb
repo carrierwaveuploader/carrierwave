1dbee3575bda4e767b2faa87b5ad56c776c561b4
Jonas Nicklas <jonas.nicklas@gmail.com>
2010-07-20 20:39:14 +0200

    Work around Datamapper not triggering callbacks for non-dirty objects(cherry picked from commit cce516117642e2aedbbd4065039f3cd11674eb90)

--- lib/carrierwave/orm/datamapper.rb
+++ lib/carrierwave/orm/datamapper.rb
@@ -19,6 +19,16 @@ module CarrierWave
       pre_hook = ::DataMapper.const_defined?(:Validate) ? :valid? : :save
       before pre_hook, "write_#{column}_identifier".to_sym
       after :destroy, "remove_#{column}!".to_sym
+
+      # FIXME: Hack to work around Datamapper not triggering callbacks
+      # for objects that are not dirty. By explicitly calling
+      # attribute_set we are marking the record as dirty.
+      class_eval <<-RUBY
+        def remove_image=(value)
+          _mounter(:#{column}).remove = value
+          attribute_set(:#{column}, '') if _mounter(:#{column}).remove?
+        end
+      RUBY
     end
 
   end # DataMapper
