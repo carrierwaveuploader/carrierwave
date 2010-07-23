c089f45b6560e1d1041a0bd89ad1632e3471a9d1
Jeroen van Dijk <jeroen@jeevidee.nl>
2010-07-23 18:23:00 +0200

    Moved ActiveRecord validations to a new ActiveModel validations modules. Both ActiveRecord and Mongoid have validations now.
    The cucumber features are passing. Unfortunately I haven't got the specs run locally due to a RubyInline error when doing `rake spec`.

--- lib/carrierwave/orm/mongoid.rb
+++ lib/carrierwave/orm/mongoid.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 require 'mongoid'
+require File.expand_path(File.dirname(__FILE__) + "../../validations/active_model")
 
 module CarrierWave
   module Mongoid
@@ -10,9 +11,17 @@ module CarrierWave
     def mount_uploader(column, uploader, options={}, &block)
       options[:mount_on] ||= "#{column}_filename"
       field options[:mount_on]
+      
       super
+      
       alias_method :read_uploader, :read_attribute
       alias_method :write_uploader, :write_attribute
+      
+      include CarrierWave::Validations::ActiveModel
+
+      validates_integrity_of  column if uploader_option(column.to_sym, :validate_integrity)
+      validates_processing_of column if uploader_option(column.to_sym, :validate_processing)
+      
       after_save "store_#{column}!".to_sym
       before_save "write_#{column}_identifier".to_sym
       after_destroy "remove_#{column}!".to_sym
