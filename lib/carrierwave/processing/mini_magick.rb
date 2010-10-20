92213faead44e6d621a196689db918f7a7c0be9f
Trevor Turk <trevorturk@gmail.com>
2010-10-20 09:54:14 +0100

    Fix to put all Minimagick at top level

--- lib/carrierwave/processing/mini_magick.rb
+++ lib/carrierwave/processing/mini_magick.rb
@@ -253,7 +253,7 @@ module CarrierWave
       image = yield(image)
       image.write(current_path)
       ::MiniMagick::Image.open(current_path)
-    rescue ::MiniMagick::Error, MiniMagick::Invalid => e
+    rescue ::MiniMagick::Error, ::MiniMagick::Invalid => e
       raise CarrierWave::ProcessingError.new("Failed to manipulate with MiniMagick, maybe it is not an image? Original Error: #{e}")
     end
 
