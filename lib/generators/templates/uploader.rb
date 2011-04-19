9b6dab637df3264311b974e953044c3453ca90cf
Oge Nnadi <ogechi.nnadi@digitalscientists.com>
2011-04-19 09:43:52 -0700

    removed s3 comment since it is no longer supported

--- lib/generators/templates/uploader.rb
+++ lib/generators/templates/uploader.rb
@@ -8,7 +8,7 @@ class <%= class_name %>Uploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :s3
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
