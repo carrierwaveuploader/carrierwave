00a41fb1952c4d75c8eafa0cf20f3d32712b53e4
did <didier@nocoffee.fr>
2011-06-08 17:43:12 +0200

    remove commented test

--- spec/orm/mongoid_spec.rb
+++ spec/orm/mongoid_spec.rb
@@ -249,7 +249,6 @@ describe CarrierWave::Mongoid do
         @doc.image_changed?.should be_false
         @doc.image = stub_file("test.jpg")
         @doc.image_changed?.should be_true
-        # @doc.changed_for_autosave?.should be_true
       end
 
     end
