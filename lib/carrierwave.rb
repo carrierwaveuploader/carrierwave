a615820f97992fac8b928d7a48d3633641a904ee
Luke Carpenter <luke.carpenter@appoxy.com>
2011-07-26 16:48:37 +0200

    Check to see if Rails~::Railtie is defined over Rails, as in some situations, Rails::Railtie isn't available but Rails is, meaning big boo-boo

--- lib/carrierwave.rb
+++ lib/carrierwave.rb
@@ -86,7 +86,7 @@ if defined?(Merb)
     Dir.glob(File.join(Merb.load_paths[:uploaders])).each {|f| require f }
   end
 
-elsif defined?(Rails)
+elsif defined?(Rails::Railtie)
 
   module CarrierWave
     class Railtie < Rails::Railtie
