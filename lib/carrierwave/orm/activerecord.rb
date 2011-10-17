04ea7cc9bbbed9488aad1a0cae8c9c72415f9754
hadins <hadins@gmail.com>
2011-10-17 17:13:24 +0700

    Update lib/carrierwave/orm/activerecord.rb

--- lib/carrierwave/orm/activerecord.rb
+++ lib/carrierwave/orm/activerecord.rb
@@ -22,6 +22,8 @@ module CarrierWave
       after_save "store_#{column}!"
       before_save "write_#{column}_identifier"
       after_destroy "remove_#{column}!"
+      before_update :"store_previous_model_for_#{column}"
+      after_save :"remove_previously_stored_#{column}"
     end
 
     ##
