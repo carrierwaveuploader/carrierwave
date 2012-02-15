b44063d502be42db86c207984f6c2bb60c2b56d6
James Miller <bensie@gmail.com>
2012-02-15 08:22:27 -0800

    Add CarrierWave::Storage::Fog::File#exists?, closes #623

--- lib/carrierwave/storage/fog.rb
+++ lib/carrierwave/storage/fog.rb
@@ -229,6 +229,16 @@ module CarrierWave
         end
 
         ##
+        # Check if the file exists on the remote service
+        #
+        # === Returns
+        #
+        # [Boolean] true if file exists or false
+        def exists?
+          !!directory.files.head(path)
+        end
+
+        ##
         # Write file to service
         #
         # === Returns
