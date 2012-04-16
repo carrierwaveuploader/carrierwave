7b1060b42cba459e6a4bfa93135dd354f2a9a2f8
Tanzeeb Khalili <tanzeeb@xtremelabs.com>
2012-04-16 17:23:13 -0400

    Raise error when downloading a missing file
    Added support for recording download errors
    
    Download validations
    
    Update documentation to reflect download validation translations
    
    Added ActiveModel validations support for remote files

--- lib/carrierwave/uploader/configuration.rb
+++ lib/carrierwave/uploader/configuration.rb
@@ -31,8 +31,10 @@ module CarrierWave
         # Mounting
         add_config :ignore_integrity_errors
         add_config :ignore_processing_errors
+        add_config :ignore_download_errors
         add_config :validate_integrity
         add_config :validate_processing
+        add_config :validate_download
         add_config :mount_on
 
         # set default values
@@ -120,8 +122,10 @@ module CarrierWave
             config.remove_previously_stored_files_after_update = true
             config.ignore_integrity_errors = true
             config.ignore_processing_errors = true
+            config.ignore_download_errors = true
             config.validate_integrity = true
             config.validate_processing = true
+            config.validate_download = true
             config.root = lambda { CarrierWave.root }
             config.base_path = CarrierWave.base_path
             config.enable_processing = true
