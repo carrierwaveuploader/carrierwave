carrierwaveuploader/carrierwave

commit 0df712cd5baefd7ef8fe0d351dbb0dbe216a0d50

Fix bug in #remove_#{column} when filename is set explicitly

lib/carrierwave/mount.rb
@@ -296,11 +296,11 @@ module CarrierWave
       end
 
       def write_identifier
-        if remove?
-          record.write_uploader(serialization_column, '')
-        elsif not uploader.identifier.blank?
-          record.write_uploader(serialization_column, uploader.identifier)
-        end
+        return if record.frozen? || uploader.identifier.blank?
+
+        value = remove? ? '' : uploader.identifier
+
+        record.write_uploader(serialization_column, value)
       end
 
       def identifier

spec/orm/activerecord_spec.rb
@@ -419,6 +419,20 @@ describe CarrierWave::ActiveRecord do
 
     describe '#destroy' do
 
+      it "should not raise an error with a custom filename" do
+        @uploader.class_eval do 
+          def filename 
+            "page.jpeg"
+          end
+        end
+
+        @event.image = stub_file('test.jpeg')
+        @event.save.should be_true
+        expect {
+          @event.destroy
+        }.to_not raise_error
+      end
+
       it "should do nothing when no file has been assigned" do
         @event.save.should be_true
         @event.destroy

commit ab033ea6bf07371248c93c8a8985c2f07ae81480

Fix regression in 0df712cd5baefd7e
-- 0df712cd5baefd7ef8fe0d351dbb0dbe216a0d50 introduced a regression
   for removal of uploads. We need to handle the case were the
   image identifier is empty, but remove_image is set.

lib/carrierwave/mount.rb
@@ -296,11 +296,13 @@ module CarrierWave
       end
 
       def write_identifier
-        return if record.frozen? || uploader.identifier.blank?
+        return if record.frozen?
 
-        value = remove? ? '' : uploader.identifier
-
-        record.write_uploader(serialization_column, value)
+        if remove?
+          record.write_uploader(serialization_column, '')
+        elsif not uploader.identifier.blank?
+          record.write_uploader(serialization_column, uploader.identifier)
+        end
       end
 
       def identifier