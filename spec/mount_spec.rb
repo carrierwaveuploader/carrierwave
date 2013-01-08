762fc0079210b0e183490cb4a46914ae55e43d71
James Miller <bensie@gmail.com>
2013-01-08 10:41:43 -0800

    Fix spec, take 2

--- spec/mount_spec.rb
+++ spec/mount_spec.rb
@@ -43,8 +43,8 @@ describe CarrierWave::Mount do
     end
 
     it "should allow marshalling uploaders and versions" do
-      Object.const_set("MyClass#{@class.object_id}", @class).gsub('-', '_')
-      Object.const_set("Uploader#{@uploader.object_id}", @uploader).gsub('-', '_')
+      Object.const_set("MyClass#{@class.object_id}".gsub('-', '_'), @class)
+      Object.const_set("Uploader#{@uploader.object_id}".gsub('-', '_'), @uploader)
       @uploader.class_eval do
         def rotate
         end
