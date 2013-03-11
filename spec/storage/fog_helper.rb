96de552f1ba62485b2c1ce5533fa4fc8761bc4e2
Gautam Punhani <gautamp@thoughtworks.com>
2013-03-12 00:03:59 +0530

    Added copy_to to copy a file across buckets in cloud

--- spec/storage/fog_helper.rb
+++ spec/storage/fog_helper.rb
@@ -247,6 +247,26 @@ end
           end
         end
 
+        describe '#copy_to' do
+          before do
+            @uploader.stub!(:store_path).and_return('uploads/test+.jpg')
+            @fog_file = @storage.store!(@file)
+            @copied_carrierwave_dir_name = "copied-#{CARRIERWAVE_DIRECTORY}"
+            @target_uploader = @uploader.new
+            @target_uploader.stub!(:store_path).with('test+copied.jpg').and_return("copied_uploads/test_copied.jpg")
+            @target_uploader.stub!(:fog_directory).and_return(@copied_carrierwave_dir_name)
+            @copy_directory = @storage.connection.directories.get(@copied_carrierwave_dir_name) || @storage.connection.directories.create(:key => @copied_carrierwave_dir_name, :public => true)
+            @fog_file.copy_to(@target_uploader, 'test+copied.jpg')
+          end
+
+          it "should copy the file to target location" do
+            @copy_directory.files.get('copied_uploads/test_copied.jpg').body.should == 'this is stuff'
+          end
+
+          it "should set target uploader" do
+            @target_uploader.url.should include("#{@copied_carrierwave_dir_name}/copied_uploads/test_copied.jpg")
+          end
+        end
       end
 
     end
