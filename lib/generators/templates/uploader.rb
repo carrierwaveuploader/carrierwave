c097370cc14901d9378a53cd8c144b89cef5f009
James Miller <bensie@gmail.com>
2013-05-29 09:25:05 -0700

    Add sprockets rails helper info for Rails 4

--- lib/generators/templates/uploader.rb
+++ lib/generators/templates/uploader.rb
@@ -6,9 +6,12 @@ class <%= class_name %>Uploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-  # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
+  # Include the Sprockets helpers for Rails 3.1 and 3.2 asset pipeline compatibility:
   # include Sprockets::Helpers::RailsHelper
   # include Sprockets::Helpers::IsolatedHelper
+
+  # Include the sprockets-rails helper for Rails 4+ asset pipeline compatibility:
+  # include Sprockets::Rails::Helper
 
   # Choose what kind of storage to use for this uploader:
   storage :file
