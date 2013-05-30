3c5cff9106b59c2b88524508e4f7f3a550d16ab5
Jason Woodward <jason@recovend.com>
2013-05-30 08:06:48 -0400

    fix busted tests on ruby 1.8.7 (1.9.3 also re-checked)

--- spec/processing/rmagick_spec.rb
+++ spec/processing/rmagick_spec.rb
@@ -21,7 +21,7 @@ describe CarrierWave::RMagick do
   describe '#convert' do
     it "should convert the image to the given format" do
       @instance.convert(:png)
-      ::Magick::Image.read(@instance.current_path).to_s.should =~ /PNG/
+      ::Magick::Image.read(@instance.current_path).first.format.should == 'PNG'
     end
   end
 
@@ -29,14 +29,14 @@ describe CarrierWave::RMagick do
     it "should resize the image to exactly the given dimensions and maintain file type" do
       @instance.resize_to_fill(200, 200)
       @instance.should have_dimensions(200, 200)
-      ::Magick::Image.read(@instance.current_path).to_s.should =~ /JPEG/
+      ::Magick::Image.read(@instance.current_path).first.format.should == 'JPEG'
     end
 
     it "should resize the image to exactly the given dimensions and maintain updated file type" do
       @instance.convert('png')
       @instance.resize_to_fill(200, 200)
       @instance.should have_dimensions(200, 200)
-      ::Magick::Image.read(@instance.current_path).to_s.should =~ /PNG/
+      ::Magick::Image.read(@instance.current_path).first.format.should == 'PNG'
     end
 
     it "should scale up the image if it smaller than the given dimensions" do
@@ -49,14 +49,14 @@ describe CarrierWave::RMagick do
     it "should resize the image to exactly the given dimensions and maintain file type" do
       @instance.resize_and_pad(200, 200)
       @instance.should have_dimensions(200, 200)
-      ::Magick::Image.read(@instance.current_path).to_s.should =~ /JPEG/
+      ::Magick::Image.read(@instance.current_path).first.format.should == 'JPEG'
     end
 
     it "should resize the image to exactly the given dimensions and maintain updated file type" do
       @instance.convert('png')
       @instance.resize_and_pad(200, 200)
       @instance.should have_dimensions(200, 200)
-      ::Magick::Image.read(@instance.current_path).to_s.should =~ /PNG/
+      ::Magick::Image.read(@instance.current_path).first.format.should == 'PNG'
     end
 
     it "should pad with white" do
@@ -91,14 +91,14 @@ describe CarrierWave::RMagick do
     it "should resize the image to fit within the given dimensions and maintain file type" do
       @instance.resize_to_fit(200, 200)
       @instance.should have_dimensions(200, 150)
-      ::Magick::Image.read(@instance.current_path).to_s.should =~ /JPEG/
+      ::Magick::Image.read(@instance.current_path).first.format.should == 'JPEG'
     end
 
     it "should resize the image to fit within the given dimensions and maintain updated file type" do
       @instance.convert('png')
       @instance.resize_to_fit(200, 200)
       @instance.should have_dimensions(200, 150)
-      ::Magick::Image.read(@instance.current_path).to_s.should =~ /PNG/
+      ::Magick::Image.read(@instance.current_path).first.format.should == 'PNG'
     end
 
     it "should scale up the image if it smaller than the given dimensions" do
@@ -111,14 +111,14 @@ describe CarrierWave::RMagick do
     it "should resize the image to fit within the given dimensions and maintain file type" do
       @instance.resize_to_limit(200, 200)
       @instance.should have_dimensions(200, 150)
-      ::Magick::Image.read(@instance.current_path).to_s.should =~ /JPEG/
+      ::Magick::Image.read(@instance.current_path).first.format.should == 'JPEG'
     end
 
     it "should resize the image to fit within the given dimensions and maintain updated file type" do
       @instance.convert('png')
       @instance.resize_to_limit(200, 200)
       @instance.should have_dimensions(200, 150)
-      ::Magick::Image.read(@instance.current_path).to_s.should =~ /PNG/
+      ::Magick::Image.read(@instance.current_path).first.format.should == 'PNG'
     end
 
     it "should not scale up the image if it smaller than the given dimensions" do
@@ -131,14 +131,14 @@ describe CarrierWave::RMagick do
     it "should resize the image to comply with `200x200^` Geometry String spec and maintain file type" do
       @instance.resize_to_geometry_string('200x200^')
       @instance.should have_dimensions(267, 200)
-      ::Magick::Image.read(@instance.current_path).to_s.should =~ /JPEG/
+      ::Magick::Image.read(@instance.current_path).first.format.should == 'JPEG'
     end
 
     it "should resize the image to comply with `200x200^` Geometry String spec and maintain updated file type" do
       @instance.convert('png')
       @instance.resize_to_geometry_string('200x200^')
       @instance.should have_dimensions(267, 200)
-      ::Magick::Image.read(@instance.current_path).to_s.should =~ /PNG/
+      ::Magick::Image.read(@instance.current_path).first.format.should == 'PNG'
     end
 
     it "should resize the image to have 125% larger dimensions" do
