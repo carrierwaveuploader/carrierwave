c04aed10b6729f71a116d362ccb20ee60422dba0
klacointe <kevinlacointe@gmail.com>
2013-09-11 19:22:00 +0200

    SanitizedFile fallback content_type from file path

--- lib/carrierwave/processing/mime_types.rb
+++ lib/carrierwave/processing/mime_types.rb
@@ -57,7 +57,7 @@ module CarrierWave
     #                      false by default
     #
     def set_content_type(override=false)
-      if override || file.content_type.blank? || generic_content_type?
+      if override || file.instance_variable_get(:@content_type).blank? || generic_content_type?
         new_content_type = ::MIME::Types.type_for(file.original_filename).first.to_s
         if file.respond_to?(:content_type=)
           file.content_type = new_content_type
