8ea59c7c685eded5d09cf7c5c163cb84db78ce1a
Joshua Kovach <kovach.jc@gmail.com>
2014-03-19 14:14:20 -0400

    Pass #url args to #default_url
    This will give us a few extra pieces to work with to name
    a certain thumbnail for something. For instance:
    
    ```ruby
    def default_url(*args)
      vname = version_name || args.first
      [model.mounted_as.to_s, vname, "default"].join("_")
    end
    ```
    
    This will allow us to call `model.attachment.url(:not_a_version)` and still
    return "attachment_not_a_version_default", which is useful in cases of
    conditional versioning.

--- lib/carrierwave/uploader/default_url.rb
+++ lib/carrierwave/uploader/default_url.rb
@@ -5,14 +5,14 @@ module CarrierWave
     module DefaultUrl
 
       def url(*args)
-        super || default_url
+        super || default_url(*args)
       end
 
       ##
       # Override this method in your uploader to provide a default url
       # in case no file has been cached/stored yet.
       #
-      def default_url; end
+      def default_url(*args); end
 
     end # DefaultPath
   end # Uploader
