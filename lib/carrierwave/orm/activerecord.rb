2f77550ed6ae4f979953259f6384c20a810d94c1
Jonas Nicklas and Lisa Hammarström <dev+jnicklas+lisarut@elabs.se>
2014-10-17 14:10:06 +0200

    Remove duplication in ActiveRecord integration

--- lib/carrierwave/orm/activerecord.rb
+++ lib/carrierwave/orm/activerecord.rb
@@ -14,45 +14,11 @@ module CarrierWave
     def mount_uploader(column, uploader=nil, options={}, &block)
       super
 
-      alias_method :read_uploader, :read_attribute
-      alias_method :write_uploader, :write_attribute
-      public :read_uploader
-      public :write_uploader
-
-      include CarrierWave::Validations::ActiveModel
-
-      validates_integrity_of column if uploader_option(column.to_sym, :validate_integrity)
-      validates_processing_of column if uploader_option(column.to_sym, :validate_processing)
-      validates_download_of column if uploader_option(column.to_sym, :validate_download)
-
-      after_save :"store_#{column}!"
-      before_save :"write_#{column}_identifier"
-      after_commit :"remove_#{column}!", :on => :destroy
-      after_commit :"mark_remove_#{column}_false", :on => :update
-      after_save :"remove_previously_stored_#{column}"
-
       class_eval <<-RUBY, __FILE__, __LINE__+1
-        def #{column}=(new_file)
-          column = _mounter(:#{column}).serialization_column
-          send(:"\#{column}_will_change!")
-          super
-        end
-
         def remote_#{column}_url=(url)
           column = _mounter(:#{column}).serialization_column
           send(:"\#{column}_will_change!")
           super
-        end
-
-        def remove_#{column}=(value)
-          send(:"#{column}_will_change!")
-          super
-        end
-
-        def remove_#{column}!
-          super
-          self.remove_#{column} = true
-          write_#{column}_identifier
         end
       RUBY
     end
@@ -61,6 +27,20 @@ module CarrierWave
     # See +CarrierWave::Mount#mount_uploaders+ for documentation
     #
     def mount_uploaders(column, uploader=nil, options={}, &block)
+      super
+
+      class_eval <<-RUBY, __FILE__, __LINE__+1
+        def remote_#{column}_urls=(url)
+          column = _mounter(:#{column}).serialization_column
+          send(:"\#{column}_will_change!")
+          super
+        end
+      RUBY
+    end
+
+  private
+
+    def mount_base(column, uploader=nil, options={}, &block)
       super
 
       alias_method :read_uploader, :read_attribute
@@ -82,12 +62,6 @@ module CarrierWave
 
       class_eval <<-RUBY, __FILE__, __LINE__+1
         def #{column}=(new_file)
-          column = _mounter(:#{column}).serialization_column
-          send(:"\#{column}_will_change!")
-          super
-        end
-
-        def remote_#{column}_urls=(url)
           column = _mounter(:#{column}).serialization_column
           send(:"\#{column}_will_change!")
           super
