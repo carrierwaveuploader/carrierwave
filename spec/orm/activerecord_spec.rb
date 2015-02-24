3cbed2cee9fe5ee02a806d93db0fd8314557f405
yui-knk <spiketeika@gmail.com>
2015-02-24 18:08:38 +0900

    Fix ArgumentError when ActiveRecord::Locking::Pessimistic#with_lock called
    This method pass one argument to reload, so reload with no args
    definition raise ArgumentError.

--- spec/orm/activerecord_spec.rb
+++ spec/orm/activerecord_spec.rb
@@ -1490,5 +1490,9 @@ describe CarrierWave::ActiveRecord do
       expect(@event.save).to be_true
       expect(File.exist?(public_path('uploads/old.jpeg'))).to be_true
     end
+
+    it "should not raise ArgumentError when with_lock method is called" do
+      expect { @event.with_lock {} }.to_not raise_error
+    end
   end
 end
