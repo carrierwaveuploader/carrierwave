e56574992842548ed6eeb5833e569c5e9bf62fb9
Benjamin Quorning <bquorning@zendesk.com>
2015-02-24 19:47:00 +0100

    Rescue LoadError if Fog cannot be required

--- lib/carrierwave/storage.rb
+++ lib/carrierwave/storage.rb
@@ -4,7 +4,10 @@ require "carrierwave/storage/file"
 begin
   require "fog/core"
 rescue LoadError
-  require "fog" unless defined?(::Fog)
+  begin
+    require "fog" unless defined?(::Fog)
+  rescue LoadError
+  end
 end
 
 require "carrierwave/storage/fog" if defined?(Fog)
