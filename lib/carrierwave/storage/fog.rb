cb5ea2d0e18684ae7ddd1f4487881f99a39c3c9b
Oleg Vivtash <oleg@vivtash.net>
2015-08-18 07:44:19 +0300

    Passing options to Rackspace authenticated_url

--- lib/carrierwave/storage/fog.rb
+++ lib/carrierwave/storage/fog.rb
@@ -189,12 +189,16 @@ module CarrierWave
             # avoid a get by using local references
             local_directory = connection.directories.new(:key => @uploader.fog_directory)
             local_file = local_directory.files.new(:key => path)
-            if @uploader.fog_credentials[:provider] == "AWS"
-              local_file.url(::Fog::Time.now + @uploader.fog_authenticated_url_expiration, options)
-            elsif ['Rackspace', 'OpenStack'].include?(@uploader.fog_credentials[:provider])
-              connection.get_object_https_url(@uploader.fog_directory, path, ::Fog::Time.now + @uploader.fog_authenticated_url_expiration)
-            else
-              local_file.url(::Fog::Time.now + @uploader.fog_authenticated_url_expiration)
+            expire_at = ::Fog::Time.now + @uploader.fog_authenticated_url_expiration
+            case @uploader.fog_credentials[:provider]
+              when 'AWS'
+                local_file.url(expire_at, options)
+              when 'Rackspace'
+                connection.get_object_https_url(@uploader.fog_directory, path, expire_at, options)
+              when 'OpenStack'
+                connection.get_object_https_url(@uploader.fog_directory, path, expire_at)
+              else
+                local_file.url(expire_at)
             end
           end
         end
