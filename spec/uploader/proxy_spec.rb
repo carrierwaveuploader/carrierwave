5b0fab4a774117bbe6c2812e54bda27b5e889e17
Thiago Fernandes Massa <thiagown@gmail.com>
2016-04-15 20:59:21 +0200

    Refactored the uploader proxy spec

--- spec/uploader/proxy_spec.rb
+++ spec/uploader/proxy_spec.rb
@@ -1,68 +1,79 @@
 require 'spec_helper'
 
 describe CarrierWave::Uploader do
+  let(:uploader_class) { Class.new(CarrierWave::Uploader::Base) }
+  let(:uploader) { uploader_class.new }
+  let(:test_file_name) { 'test.jpg' }
+  let(:test_file) { File.open(file_path(test_file_name)) }
+  let(:path) { '1369894322-345-1234-2255/test.jpeg' }
 
-  before do
-    @uploader_class = Class.new(CarrierWave::Uploader::Base)
-    @uploader = @uploader_class.new
-  end
-
-  after do
-    FileUtils.rm_rf(public_path)
-  end
+  after { FileUtils.rm_rf(public_path) }
 
   describe '#blank?' do
-    it "should be true when nothing has been done" do
-      expect(@uploader).to be_blank
+    subject { uploader }
+
+    context "when nothing has been done" do
+      it { is_expected.to be_blank }
     end
 
-    it "should not be true when the file is empty" do
-      @uploader.retrieve_from_cache!('1369894322-345-1234-2255/test.jpeg')
-      expect(@uploader).to be_blank
+    context "when file is empty" do
+      before { uploader.retrieve_from_cache!(path) }
+
+      it { is_expected.to be_blank }
     end
 
-    it "should not be true when a file has been cached" do
-      @uploader.cache!(File.open(file_path('test.jpg')))
-      expect(@uploader).not_to be_blank
+    context "when file has been cached" do
+      before { uploader.cache!(test_file) }
+
+      it { is_expected.not_to be_blank }
     end
   end
 
   describe '#read' do
-    it "should be nil by default" do
-      expect(@uploader.read).to be_nil
+    subject { uploader.read }
+
+    describe "default behavior" do
+      it { is_expected.to be nil }
     end
 
-    it "should read the contents of a cached file" do
-      @uploader.cache!(File.open(file_path('test.jpg')))
-      expect(@uploader.read).to eq("this is stuff")
+    context "when file is cached" do
+      before { uploader.cache!(test_file) }
+
+      it { is_expected.to eq("this is stuff") }
     end
   end
 
   describe '#size' do
-    it "should be zero by default" do
-      expect(@uploader.size).to eq(0)
+    subject { uploader.size }
+
+    describe "default behavior" do
+      it { is_expected.to be 0 }
     end
 
-    it "should get the size of a cached file" do
-      @uploader.cache!(File.open(file_path('test.jpg')))
-      expect(@uploader.size).to eq(13)
+    context "when file is cached" do
+      before { uploader.cache!(test_file) }
+
+      it { is_expected.to be 13 }
     end
   end
 
   describe '#content_type' do
-    it "should be nil when nothing has been done" do
-      expect(@uploader.content_type).to be_nil
+    subject { uploader.content_type }
+
+    context "when nothing has been done" do
+      it { is_expected.to be_nil }
     end
 
-    it "should get the content type when the file has been cached" do
-      @uploader.cache!(File.open(file_path('test.jpg')))
-      expect(@uploader.content_type).to eq('image/jpeg')
+    context "when the file has been cached" do
+      before { uploader.cache!(test_file) }
+
+      it { is_expected.to eq('image/jpeg') }
     end
 
-    it "should get the content type when the file is empty" do
-      @uploader.retrieve_from_cache!('1369894322-345-1234-2255/test.jpeg')
-      expect(@uploader.content_type).to eq('image/jpeg')
+    context "when the file is empty" do
+      before { uploader.retrieve_from_cache!(path) }
+
+      it { is_expected.to eq('image/jpeg') }
     end
   end
-
 end
