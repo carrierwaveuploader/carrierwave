124e756a77f0425145c4623461420adf89c06cb2
Thiago Fernandes Massa <thiagown@gmail.com>
2016-04-18 21:24:43 +0200

    Refactored uploader url spec

--- spec/uploader/url_spec.rb
+++ spec/uploader/url_spec.rb
@@ -3,213 +3,271 @@ require 'active_support/json'
 
 describe CarrierWave::Uploader do
 
-  before do
-    class MyCoolUploader < CarrierWave::Uploader::Base; end
-    @uploader = MyCoolUploader.new
-  end
+  let(:uploader) { MyCoolUploader.new }
+
+  before { class MyCoolUploader < CarrierWave::Uploader::Base; end }
 
   after do
     FileUtils.rm_rf(public_path)
     Object.send(:remove_const, "MyCoolUploader") if defined?(::MyCoolUploader)
   end
 
+  let(:cache_id) { '1369894322-345-1234-2255' }
+  let(:test_file) { File.open(file_path(test_file_name)) }
+  let(:test_file_name) { 'test.jpg' }
+
+  before { allow(CarrierWave).to receive(:generate_cache_id).and_return(cache_id) }
+
   describe '#url' do
-    before do
-      allow(CarrierWave).to receive(:generate_cache_id).and_return('1369894322-345-1234-2255')
+    subject(:url) { uploader.url }
+
+    it { is_expected.to be_nil }
+
+    it "doesn't raise exception when hash specified as argument" do
+      expect { uploader.url({}) }.not_to raise_error
     end
 
-    it "should default to nil" do
-      expect(@uploader.url).to be_nil
+    it "encodes the path of a file without an asset host" do
+      uploader.cache!(File.open(file_path('test+.jpg')))
+      is_expected.to eq("/uploads/tmp/#{cache_id}/test%2B.jpg")
     end
 
-    it "should raise ArgumentError when version doesn't exist" do
-      expect { @uploader.url(:thumb) }.to raise_error(ArgumentError)
-    end
+    context "with a cached file" do
+      before { uploader.cache!(test_file) }
 
-    it "should not raise exception when hash specified as argument" do
-      expect { @uploader.url({}) }.not_to raise_error
-    end
-
-    it "should not raise ArgumentError when storage's File#url method doesn't get params" do
-      module StorageX; class File; def url; true; end; end; end
-      allow(@uploader).to receive(:file).and_return(StorageX::File.new)
-      expect { @uploader.url }.not_to raise_error
-    end
-
-    it "should not raise ArgumentError when versions version exists" do
-      MyCoolUploader.version(:thumb)
-      expect { @uploader.url(:thumb) }.not_to raise_error
-    end
-
-    it "should get the directory relative to public, prepending a slash" do
-      @uploader.cache!(File.open(file_path('test.jpg')))
-      expect(@uploader.url).to eq('/uploads/tmp/1369894322-345-1234-2255/test.jpg')
-    end
-
-    it "should get the directory relative to public for a specific version" do
-      MyCoolUploader.version(:thumb)
-      @uploader.cache!(File.open(file_path('test.jpg')))
-      expect(@uploader.url(:thumb)).to eq('/uploads/tmp/1369894322-345-1234-2255/thumb_test.jpg')
-    end
-
-    it "should get the directory relative to public for a nested version" do
-      MyCoolUploader.version(:thumb) do
-        version(:mini)
+      it "gets the directory relative to public, prepending a slash" do
+        is_expected.to eq("/uploads/tmp/#{cache_id}/#{test_file_name}")
       end
-      @uploader.cache!(File.open(file_path('test.jpg')))
-      expect(@uploader.url(:thumb, :mini)).to eq('/uploads/tmp/1369894322-345-1234-2255/thumb_mini_test.jpg')
-    end
 
-    it "should prepend the config option 'asset_host', if set and a string" do
-      MyCoolUploader.version(:thumb)
-      @uploader.class.configure do |config|
-        config.asset_host = "http://foo.bar"
+      describe "File#url" do
+        before do
+          allow(uploader.file).to receive(:url).and_return(file_url)
+        end
+
+        context "when present" do
+          let(:file_url) { 'http://www.example.com/someurl.jpg' }
+
+          it { is_expected.to eq(file_url) }
+        end
+
+        context "when blank" do
+          let(:file_url) { '' }
+
+          it "returns the relative path" do
+            is_expected.to eq("/uploads/tmp/#{cache_id}/#{test_file_name}")
+          end
+        end
       end
-      @uploader.cache!(File.open(file_path('test.jpg')))
-      expect(@uploader.url(:thumb)).to eq('http://foo.bar/uploads/tmp/1369894322-345-1234-2255/thumb_test.jpg')
     end
 
-    it "should prepend the result of the config option 'asset_host', if set and a proc" do
-      MyCoolUploader.version(:thumb)
-      @uploader.class.configure do |config|
-        config.asset_host = proc { "http://foo.bar" }
+    context "when File#url method doesn't get params" do
+      before do
+        module StorageX
+          class File
+            def url
+              true
+            end
+          end
+        end
+
+        allow(uploader).to receive(:file).and_return(StorageX::File.new)
       end
-      @uploader.cache!(File.open(file_path('test.jpg')))
-      expect(@uploader.url(:thumb)).to eq('http://foo.bar/uploads/tmp/1369894322-345-1234-2255/thumb_test.jpg')
-    end
 
-    it "should prepend the config option 'base_path', if set and 'asset_host' is not set" do
-      MyCoolUploader.version(:thumb)
-      @uploader.class.configure do |config|
-        config.base_path = "/base_path"
-        config.asset_host = nil
+      it "raises ArgumentError" do
+        expect { uploader.url }.not_to raise_error
       end
-      @uploader.cache!(File.open(file_path('test.jpg')))
-      expect(@uploader.url(:thumb)).to eq('/base_path/uploads/tmp/1369894322-345-1234-2255/thumb_test.jpg')
     end
 
-    it "should return file#url if available" do
-      @uploader.cache!(File.open(file_path('test.jpg')))
-      allow(@uploader.file).to receive(:url).and_return('http://www.example.com/someurl.jpg')
-      expect(@uploader.url).to eq('http://www.example.com/someurl.jpg')
-    end
+    describe "(:thumb)" do
+      subject { uploader.url(:thumb) }
 
-    it "should get the directory relative to public, if file#url is blank" do
-      @uploader.cache!(File.open(file_path('test.jpg')))
-      allow(@uploader.file).to receive(:url).and_return('')
-      expect(@uploader.url).to eq('/uploads/tmp/1369894322-345-1234-2255/test.jpg')
-    end
-
-    it "should uri encode the path of a file without an asset host" do
-      @uploader.cache!(File.open(file_path('test+.jpg')))
-      expect(@uploader.url).to eq('/uploads/tmp/1369894322-345-1234-2255/test%2B.jpg')
-    end
-
-    it "should uri encode the path of a file with a string asset host" do
-      MyCoolUploader.version(:thumb)
-      @uploader.class.configure do |config|
-        config.asset_host = "http://foo.bar"
+      it "raises ArgumentError when version doesn't exist" do
+        expect { uploader.url(:thumb) }.to raise_error(ArgumentError)
       end
-      @uploader.cache!(File.open(file_path('test+.jpg')))
-      expect(@uploader.url(:thumb)).to eq('http://foo.bar/uploads/tmp/1369894322-345-1234-2255/thumb_test%2B.jpg')
-    end
 
-    it "should uri encode the path of a file with a proc asset host" do
-      MyCoolUploader.version(:thumb)
-      @uploader.class.configure do |config|
-        config.asset_host = proc { "http://foo.bar" }
+      context "when version is specified" do
+        before do
+          MyCoolUploader.version(:thumb)
+          uploader.cache!(test_file)
+        end
+
+        it "doesn't raise ArgumentError when versions version exists" do
+          expect { uploader.url(:thumb) }.not_to raise_error
+        end
+
+        it "gets the directory relative to public for a specific version" do
+          is_expected.to eq("/uploads/tmp/#{cache_id}/thumb_#{test_file_name}")
+        end
+
+        describe "asset_host" do
+          before { uploader.class.configure { |config| config.asset_host = asset_host } }
+
+          context "when set as a string" do
+            let(:asset_host) { "http://foo.bar" }
+
+            it "prepends the string" do
+              is_expected.to eq("#{asset_host}/uploads/tmp/#{cache_id}/thumb_#{test_file_name}")
+            end
+
+            describe "encoding" do
+              let(:test_file_name) { 'test+.jpg' }
+
+              it "encodes the path of a file" do
+                is_expected.to eq("#{asset_host}/uploads/tmp/#{cache_id}/thumb_test%2B.jpg")
+              end
+
+              it "double-encodes the path of an available File#url" do
+                url = 'http://www.example.com/directory%2Bname/another%2Bdirectory/some%2Burl.jpg'
+                allow(uploader.file).to receive(:url).and_return(url)
+
+                expect(uploader.url).to eq(url)
+              end
+            end
+          end
+
+          context "when set as a proc" do
+            let(:asset_host) { proc { "http://foo.bar" } }
+
+            it "prepends the result of proc" do
+              is_expected.to eq("#{asset_host.call}/uploads/tmp/#{cache_id}/thumb_#{test_file_name}")
+            end
+
+            describe "encoding" do
+              let(:test_file_name) { 'test+.jpg' }
+
+              it { is_expected.to eq("#{asset_host.call}/uploads/tmp/#{cache_id}/thumb_test%2B.jpg") }
+            end
+          end
+
+          context "when set as nil" do
+            let(:asset_host) { nil }
+
+            context "when base_path is set" do
+              let(:base_path) { "/base_path" }
+
+              before do
+                uploader.class.configure do |config|
+                  config.base_path = base_path
+                end
+              end
+
+              it "prepends the config option 'base_path'" do
+                is_expected.to eq("#{base_path}/uploads/tmp/#{cache_id}/thumb_#{test_file_name}")
+              end
+            end
+          end
+        end
       end
-      @uploader.cache!(File.open(file_path('test+.jpg')))
-      expect(@uploader.url(:thumb)).to eq('http://foo.bar/uploads/tmp/1369894322-345-1234-2255/thumb_test%2B.jpg')
-    end
 
-    it "shouldn't double-encode the path of an available file#url" do
-      url = 'http://www.example.com/directory%2Bname/another%2Bdirectory/some%2Burl.jpg'
-      @uploader.cache!(File.open(file_path('test.jpg')))
-      allow(@uploader.file).to receive(:url).and_return(url)
-      expect(@uploader.url).to eq(url)
+      context "when the version is nested" do
+        subject { uploader.url(:thumb, :mini) }
+
+        before do
+          MyCoolUploader.version(:thumb) { version(:mini) }
+          uploader.cache!(test_file)
+        end
+
+        it "gets the directory relative to public for a nested version" do
+          is_expected.to eq("/uploads/tmp/#{cache_id}/thumb_mini_#{test_file_name}")
+        end
+      end
     end
   end
 
   describe '#to_json' do
-    before do
-      allow(CarrierWave).to receive(:generate_cache_id).and_return('1369894322-345-1234-2255')
+    subject(:parsed_json) { JSON.parse(to_json) }
+
+    let(:to_json) { uploader.to_json }
+
+    context "(:thumb)" do
+      before { MyCoolUploader.version(:thumb) }
+
+      it { expect(parsed_json.keys).to include("url") }
+      it { expect(parsed_json.keys).to include("thumb") }
+      it { expect(parsed_json["url"]).to be_nil }
+      it { expect(parsed_json["thumb"].keys).to include("url") }
+      it { expect(parsed_json["thumb"]["url"]).to be_nil }
+
+      context "with a cached_file" do
+        before { uploader.cache!(test_file) }
+
+        it { expect(parsed_json.keys).to include("thumb") }
+        it { expect(parsed_json["thumb"]).to eq({"url" => "/uploads/tmp/#{cache_id}/thumb_#{test_file_name}"}) }
+      end
     end
 
-    it "should return a hash with a nil URL" do
-      MyCoolUploader.version(:thumb)
-      hash = JSON.parse(@uploader.to_json)
-      expect(hash.keys).to include("url")
-      expect(hash.keys).to include("thumb")
-      expect(hash["url"]).to be_nil
-      expect(hash["thumb"].keys).to include("url")
-      expect(hash["thumb"]["url"]).to be_nil
+    context "with cached file" do
+      before { uploader.cache!(test_file) }
+
+      it "returns a hash including a cached URL" do
+        is_expected.to eq({"url" => "/uploads/tmp/#{cache_id}/#{test_file_name}"})
+      end
     end
 
-    it "should return a hash including a cached URL" do
-      @uploader.cache!(File.open(file_path("test.jpg")))
-      expect(JSON.parse(@uploader.to_json)).to eq({"url" => "/uploads/tmp/1369894322-345-1234-2255/test.jpg"})
-    end
-
-    it "should return a hash including a cached URL of a version" do
-      MyCoolUploader.version(:thumb)
-      @uploader.cache!(File.open(file_path("test.jpg")))
-      hash = JSON.parse(@uploader.to_json)
-      expect(hash.keys).to include "thumb"
-      expect(hash["thumb"]).to eq({"url" => "/uploads/tmp/1369894322-345-1234-2255/thumb_test.jpg"})
-    end
-
-    it "should allow an options parameter to be passed in" do
-      expect { @uploader.to_json({:some => 'options'}) }.not_to raise_error
+    it "allows an options parameter to be passed in" do
+      expect { uploader.to_json({:some => 'options'}) }.not_to raise_error
     end
   end
 
   describe '#to_xml' do
-    before do
-      allow(CarrierWave).to receive(:generate_cache_id).and_return('1369894322-345-1234-2255')
+    subject(:parsed_xml) { Hash.from_xml(to_xml) }
+
+    let(:to_xml) { uploader.to_xml }
+
+    it "returns a hash with a blank URL" do
+      is_expected.to eq({"uploader" => {"url" => nil}})
     end
 
-    it "should return a hash with a blank URL" do
-      expect(Hash.from_xml(@uploader.to_xml)).to eq({"uploader" => {"url" => nil}})
+    context "with cached file" do
+      before { uploader.cache!(test_file) }
+
+      it "returns a hash including a cached URL" do
+        is_expected.to eq({"uploader" => {"url" => "/uploads/tmp/#{cache_id}/#{test_file_name}"}})
+      end
+
+      context "with an array of uploaders" do
+        let(:to_xml) { [uploader].to_xml }
+
+        it "returns a hash including an array with a cached URL" do
+          is_expected.to have_value([{"url"=>"/uploads/tmp/#{cache_id}/#{test_file_name}"}])
+        end
+      end
     end
 
-    it "should return a hash including a cached URL" do
-      @uploader.cache!(File.open(file_path("test.jpg")))
-      expect(Hash.from_xml(@uploader.to_xml)).to eq({"uploader" => {"url" => "/uploads/tmp/1369894322-345-1234-2255/test.jpg"}})
-    end
+    describe "(:thumb)" do
+      before { MyCoolUploader.version(:thumb) }
 
-    it "should return a hash including a cached URL of a version" do
-      MyCoolUploader.version(:thumb)
-      @uploader.cache!(File.open(file_path("test.jpg")))
-      expect(Hash.from_xml(@uploader.to_xml)["uploader"]["thumb"]).to eq({"url" => "/uploads/tmp/1369894322-345-1234-2255/thumb_test.jpg"})
-    end
+      context "with cached file" do
+        before { uploader.cache!(test_file) }
 
-    it "should return a hash including an array with a cached URL" do
-      @uploader.cache!(File.open(file_path("test.jpg")))
-      hash = Hash.from_xml([@uploader].to_xml)
-      expect(hash).to have_value([{"url"=>"/uploads/tmp/1369894322-345-1234-2255/test.jpg"}])
+        it "returns a hash including a cached URL of a version" do
+          expect(parsed_xml["uploader"]["thumb"]).to eq({"url" => "/uploads/tmp/#{cache_id}/thumb_#{test_file_name}"})
+        end
+      end
     end
   end
 
   describe '#to_s' do
-    before do
-      allow(CarrierWave).to receive(:generate_cache_id).and_return('1369894322-345-1234-2255')
-    end
+    subject { uploader.to_s }
 
-    it "should default to empty space" do
-      expect(@uploader.to_s).to eq('')
-    end
+    it { is_expected.to eq('') }
 
-    it "should get the directory relative to public, prepending a slash" do
-      @uploader.cache!(File.open(file_path('test.jpg')))
-      expect(@uploader.to_s).to eq('/uploads/tmp/1369894322-345-1234-2255/test.jpg')
-    end
+    context "with cached file" do
+      before { uploader.cache!(test_file) }
 
-    it "should return file#url if available" do
-      @uploader.cache!(File.open(file_path('test.jpg')))
-      allow(@uploader.file).to receive(:url).and_return('http://www.example.com/someurl.jpg')
-      expect(@uploader.to_s).to eq('http://www.example.com/someurl.jpg')
+      it "gets the directory relative to public, prepending a slash" do
+        is_expected.to eq("/uploads/tmp/#{cache_id}/#{test_file_name}")
+      end
+
+      describe "File#url" do
+        before { allow(uploader.file).to receive(:url).and_return(url) }
+
+        context "when present" do
+          let(:url) { 'http://www.example.com/someurl.jpg' }
+
+          it { is_expected.to eq(url) }
+        end
+      end
     end
   end
-
 end
