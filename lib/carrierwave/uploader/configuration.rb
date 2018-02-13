9020604a837a692668bc4337ba03a2ac91c4b15f
Akira Matsuda <ronnie@dio.jp>
2018-02-13 19:48:04 +0900

    Initialize an ivar with add_config to supress warnings

--- lib/carrierwave/uploader/configuration.rb
+++ lib/carrierwave/uploader/configuration.rb
@@ -117,6 +117,8 @@ module CarrierWave
 
         def add_config(name)
           class_eval <<-RUBY, __FILE__, __LINE__ + 1
+            @#{name} = nil
+
             def self.eager_load_fog(fog_credentials)
               # see #1198. This will hopefully no longer be necessary after fog 2.0
               require self.fog_provider
