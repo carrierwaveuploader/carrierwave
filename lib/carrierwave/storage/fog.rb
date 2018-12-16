d3d11516fad3d0d48b4b97bf12081af7e6ea7674
Stan Hu <stanhu@gmail.com>
2018-12-16 07:21:31 -0800

    Check if options parameter is available in :url method
    This is to ensure older versions of fog-google don't break.

--- lib/carrierwave/storage/fog.rb
+++ lib/carrierwave/storage/fog.rb
@@ -193,7 +193,13 @@ module CarrierWave
             expire_at = ::Fog::Time.now + @uploader.fog_authenticated_url_expiration
             case @uploader.fog_credentials[:provider]
               when 'AWS', 'Google'
-                local_file.url(expire_at, options)
+                # Older versions of fog-google do not support options as a parameter
+                if url_options_supported?(local_file)
+                  local_file.url(expire_at, options)
+                else
+                  warn "Options hash not supported in #{local_file.class}. You may need to upgrade your Fog provider."
+                  local_file.url(expire_at)
+                end
               when 'Rackspace'
                 connection.get_object_https_url(@uploader.fog_directory, path, expire_at, options)
               when 'OpenStack'
@@ -486,6 +492,11 @@ module CarrierWave
           ensure
             file_body.close
           end
+        end
+
+        def url_options_supported?(local_file)
+          parameters = file.method(:url).parameters
+          parameters.count == 2 && parameters[1].include?(:options)
         end
       end
 
