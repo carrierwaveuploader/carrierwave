88f24451352bda128825f857cde473107d98fca7
M.Shibuya <mit.shibuya@gmail.com>
2021-01-17 15:55:05 +0900

    Support the latest version of rmagick

--- lib/carrierwave/processing/rmagick.rb
+++ lib/carrierwave/processing/rmagick.rb
@@ -228,7 +228,7 @@ module CarrierWave
       height = dimension_from height
       manipulate! do |img|
         img.resize_to_fit!(width, height)
-        new_img = ::Magick::Image.new(width, height) { self.background_color = background == :transparent ? 'rgba(255,255,255,0)' : background.to_s }
+        new_img = ::Magick::Image.new(width, height) { |img| img.background_color = background == :transparent ? 'rgba(255,255,255,0)' : background.to_s }
         if background == :transparent
           filled = new_img.matte_floodfill(1, 1)
         else
@@ -378,7 +378,7 @@ module CarrierWave
 
     def create_info_block(options)
       return nil unless options
-      assignments = options.map { |k, v| "self.#{k} = #{v}" }
+      assignments = options.map { |k, v| "img.#{k} = #{v}" }
       code = "lambda { |img| " + assignments.join(";") + "}"
       eval code
     end
