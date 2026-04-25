21221cc6e260633f7da78c6133a88666a5529d27
Mitsuhiro Shibuya <mit.shibuya@gmail.com>
2026-04-25 18:13:29 +0900

    Security: Fix bypass in content_type_denylist via unescaped RegExp chars
    Refs. https://github.com/carrierwaveuploader/carrierwave/security/advisories/GHSA-7g26-2qgj-chfg

--- spec/uploader/content_type_denylist_spec.rb
+++ spec/uploader/content_type_denylist_spec.rb
@@ -54,6 +54,28 @@ describe CarrierWave::Uploader do
 
           expect { uploader.cache!(ruby_file) }.to raise_error(CarrierWave::IntegrityError)
         end
+
+        it "properly escapes metacharacters" do
+          allow(uploader).to receive(:content_type_denylist).and_return(['image/svg+xml'])
+
+          sanitized_file = CarrierWave::SanitizedFile.new(ruby_file)
+          allow(sanitized_file).to receive(:content_type).and_return('image/svg+xml')
+
+          expect { uploader.send(:check_content_type_denylist!, sanitized_file) }.to raise_error(CarrierWave::IntegrityError)
+        end
+
+        it "matches anywhere in the string to be more restrictive" do
+          allow(uploader).to receive(:content_type_denylist).and_return(['html'])
+
+          sanitized_file = CarrierWave::SanitizedFile.new(ruby_file)
+          allow(sanitized_file).to receive(:content_type).and_return('text/html')
+
+          expect { uploader.send(:check_content_type_denylist!, sanitized_file) }.to raise_error(CarrierWave::IntegrityError)
+
+          allow(sanitized_file).to receive(:content_type).and_return('application/html')
+
+          expect { uploader.send(:check_content_type_denylist!, sanitized_file) }.to raise_error(CarrierWave::IntegrityError)
+        end
       end
 
       context "when the denylist is a single value" do
