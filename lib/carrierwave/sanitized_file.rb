cfc3364f5b046686e9b1e1f7f995d94748ef9e3d
Igor_Khlebnikoff <igor@trainual.com>
2026-08-31 14:37:03 +0300

    fix_string_mb_chars_deprecation

--- lib/carrierwave/sanitized_file.rb
+++ lib/carrierwave/sanitized_file.rb
@@ -296,7 +296,7 @@ module CarrierWave
       name = name.gsub(sanitize_regexp,"_")
       name = "_#{name}" if name =~ /\A\.+\z/
       name = "unnamed" if name.size.zero?
-      return name.mb_chars.to_s
+      name
     end
 
     def existing_content_type
